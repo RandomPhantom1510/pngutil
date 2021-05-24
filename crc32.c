6b34d53bb8ac30c226ffd95c25ebae5fcec4f8e7
Zaraksh Rahman <zarakshR@pm.me>
2021-05-24 18:37:55 +0530

    Added skeleton for CRC32 implementation.

---
+++ crc32.c
@@ -0,0 +1,5 @@
+//
+// Created by zaraksh on 5/24/21.
+//
+
+#include "crc32.h"
---
+++ crc32.h
@@ -0,0 +1,8 @@
+//
+// Created by zaraksh on 5/24/21.
+//
+
+#ifndef PNGUTIL_CRC32_H
+#define PNGUTIL_CRC32_H
+
+#endif //PNGUTIL_CRC32_H
