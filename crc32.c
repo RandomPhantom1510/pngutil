da0a17dbf96b0ebd2ff07342af93f4ddea0861b4
Zaraksh Rahman <zarakshR@pm.me>
2021-05-29 19:19:02 +0530

    Added CMakeList.txt

--- crc32.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by zaraksh on 5/24/21.
-//
-
-#include "crc32.h"
--- crc32.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by zaraksh on 5/24/21.
-//
-
-#ifndef PNGUTIL_CRC32_H
-#define PNGUTIL_CRC32_H
-
-#endif //PNGUTIL_CRC32_H
